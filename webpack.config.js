4a8275b5ca93d31b5f0e19ddea40086a2011412a
ivantsov-aa <pr.minsk.ivantsov@gmail.com>
2023-09-07 12:26:00 +0300

    add copy assets to prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.js",
@@ -11,6 +12,14 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "public", "index.html"),
     }),
+    new CopyPlugin({
+      patterns: [
+          {
+              from: path.resolve(__dirname, 'public/assets'),
+              to: path.resolve(__dirname, 'build/assets')
+          }
+      ]
+  }),
   ],
   devtool: "source-map",
   devServer: {
